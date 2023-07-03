7bb17d19b8931fe5d19ae1405694219abce00e7e
Qwerty <114013507+nvvuti@users.noreply.github.com>
2023-07-03 13:10:19 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var Discord = require('discord.js');
 var client = new Discord.Client({ "intents": new Discord.IntentsBitField(130813) });
 var token = "сюда-токен-вашего-бота"
-//Нужно создать 17-18 серверов и везде применить команду !automod и подождать 24 часа что бы получить значок!
+//Нужно создать 17-18 серверов и везде применить команду !automod и подождать 24 часа, что бы получить значок!
 
 client.on("ready", () => {
     console.log(`Бот: ${client.user.username} Запустился!`);
@@ -9,7 +9,7 @@ client.on("ready", () => {
 
 client.on("messageCreate", message => {
     if (message.content.includes("!automod")) {
-        if (message.author.id != "281491756565135362") return;
+        if (message.author.id != "сюда-ваш-USER-ID") return;//Эта проверка что бы только вы могли использовать команду если хотите что бы любой мог использовать просто удалите эту строку!
         message.guild.autoModerationRules.create({
             name: `AutoMod-1`,
             creatorId: `820361590826205215`,
